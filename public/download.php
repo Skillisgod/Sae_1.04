7890a0505d74f9d414fe34d8b664960aa1237820
DamienVlvt <damien.villevet@gmail.com>
2023-01-23 20:26:03 +0100

    sae1.04

--- public/download.php
+++ public/download.php
@@ -1,9 +1,9 @@
 <?php
-$fichier = 'cv.pdf';
+$fichier = 'cv.jpg';
  
 // Création des headers, pour indiquer au navigateur qu'il s'agit d'un fichier à télécharger
   header('Content-Transfer-Encoding: binary'); //Transfert en binaire (fichier)
-  header('Content-Disposition: attachment; filename="cv.pdf"'); //Nom du fichier
+  header('Content-Disposition: attachment; filename="cv.jpg"'); //Nom du fichier
   header('Content-Length: ' . filesize($fichier)); //Taille du fichier
   
 //Envoi du fichier dont le chemin est passé en paramètre
